53e0978fb6f58af9d479ae3cadde9f20c802fdc1
garyma <garymar2007@gmail.com>
2024-08-18 14:01:37 +0200

    using measureTime API.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,17 +1,21 @@
 package org.gary
 
+import kotlin.time.measureTime
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 
-fun stopwatch(block: () -> Unit){
-    val startTime = System.nanoTime()
-    block()
-    val endTime = System.nanoTime()
-    val duration = (endTime - startTime) / 1_000_000
-    println("Execution time: $duration milliseconds")
-}
+//fun stopwatch(block: () -> Unit){
+//    val startTime = System.nanoTime()
+//    block()
+//    val endTime = System.nanoTime()
+//    val duration = (endTime - startTime) / 1_000_000
+//    println("Execution time: $duration milliseconds")
+//}
+
 fun main() {
-   stopwatch(SomeLongRunningProcess::execute)
+   val duration = measureTime(SomeLongRunningProcess::execute)
+    println("Execution time: $duration seconds")
 }
 
 object SomeLongRunningProcess {
